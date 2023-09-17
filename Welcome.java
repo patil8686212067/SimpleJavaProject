04ee98ab99f8feb797a02de2c876e491ab43419b
Anil Patil <anilrspatil1992@gmail.com>
2023-09-17 22:46:37 +0530

    commit related git and jenkin integration

--- Welcome.java
+++ Welcome.java
@@ -6,6 +6,8 @@ System.out.println("This is output of java program");
 System.out.println("The Current Date ::"+ new Date());
 System.out.println("Hello jenkin i amcoming");
 System.out.println("One more file added the project");
+System.out.println("checking git jenkins integration");
+
 
   }
 
