36a438b7188b9dddd31b0b921894e3e738d485b8
Anil Patil <anilrspatil1992@gmail.com>
2023-09-17 22:43:14 +0530

    added new file to the project

--- Welcome.java
+++ Welcome.java
@@ -5,6 +5,7 @@ System.out.println("This is input of java program");
 System.out.println("This is output of java program");
 System.out.println("The Current Date ::"+ new Date());
 System.out.println("Hello jenkin i amcoming");
+System.out.println("One more file added the project");
 
   }
 
